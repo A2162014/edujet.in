09cd36eb6839ba329f1e166a2d883518a76a24b1
Ashvath <ashvath.suresh@btech.christuniversity.in>
2023-02-12 13:56:11 +0530

    altered directories

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response
 app = Flask(__name__, static_folder='static')
 
+@app.route("/styles")
+def styles():
+    response = make_response(render_template("/static/styles.css"))
+    response.headers["Cache-Control"] = "no-cache"
+    return response
+
+@app.route("/script")
+def script():
+    response = make_response(render_template("/static/script.js"))
+    response.headers["Cache-Control"] = "no-cache"
+    return response
+    
 @app.route("/")
 def index():
     return render_template('index.html')
